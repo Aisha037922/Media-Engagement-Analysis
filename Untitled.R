c9042fd824b60d853e8c74b83efb90ce312d99fe
Aisha03972 <orcadolfin2005@gmail.com>
2025-05-29 13:32:29 -0700

    wow

--- Untitled.R
+++
@@ -1,4 +0,0 @@
-
-data <- read_csv("~/Marketing Data Project/Social Media Engagement Project/Social Media Engagement Dataset.csv")
-
-git
---
+++ Work.R
@@ -0,0 +1,26 @@
+#Import Data
+
+data <- read_csv("Social Media Engagement Dataset.csv")
+
+# Which platform has the highest average engagement rate?
+
+# Likes, shares, comments per post.
+
+# How does content type (text, image, video) affect engagement?
+  
+# Do posting times or days impact engagement rates?
+
+# Is there a best time to post on each platform?
+
+# How do hashtags and keywords affect reach and interaction?
+
+# Which topics or themes get the most attention across platforms?
+
+# What are the common characteristics of high-engagement posts?
+
+# Length of text, sentiment, tone, etc.
+
+# How does engagement vary by platform demographics or usage trends?
+
+# What role does sentiment (positive/negative/neutral) play in engagement?
+  
