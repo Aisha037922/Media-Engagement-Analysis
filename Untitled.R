b8ea4ded85cdfbe96888f0b5c57e5eb3201b9a52
Aisha037922 <durba472gs.2742@gmail.com>
2025-06-19 21:31:44 -0700

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -1,4 +1,64 @@
+library(tidyverse)
+library(dplyr)
 
-data <- read_csv("~/Marketing Data Project/Social Media Engagement Project/Social Media Engagement Dataset.csv")
+#Import Data
+data <- read_csv("Social Media Engagement Dataset.csv")
 
-git
+# Which platform has the highest average engagement rate? By Likes, Shares, comments and impressions per post.
+
+tot_engage <- data %>%
+  mutate(total_engagement = likes_count + shares_count + comments_count + impressions)
+
+# Show the platform with the highest average engagement
+
+ave_engage <- tot_engage%>%
+  group_by(platform) %>%
+  summarise(engagement_rate = mean(total_engagement, na.rm = TRUE)) 
+
+print(ave_engage)
+
+# What day of the week sees the highest average engagement rate across platforms?
+
+post_time_engage <- data %>%
+  group_by(day_of_week) %>%
+  summarise(ave_engage = mean(engagement_rate, na.rm = TRUE))
+
+print(post_time_engage)
+
+# Is there a best time to post on each platform? 
+
+best_time <- data %>%
+  mutate(hour = hour(timestamp)) %>%
+  group_by(timestamp) %>%
+  summarise(impressions = mean(impressions, na.rm = TRUE)) %>%
+  arrange(platform, desc(avg_impressions))
+
+print(best_time)
+
+# Which locations have the highest average engagement rate? based on topic_category, product_name and brand_name?
+loc_av <- data %>%
+  group_by(location, topic_category, product_name, brand_name) %>%
+  summarise(engagement_rate = mean(engagement_rate, na.rm = TRUE)) %>%
+  arrange(desc(engagement_rate)) %>%
+  slice(1)
+
+print(loc_av)
+
+# How does user_past_sentiment_avg impact current post sentiment or engagement rate ?
+
+senti_level <- data %>% 
+  group_by(sentiment_label, emotion_type, user_engagement_growth, user_past_sentiment_avg, engagement_rate) %>%
+  summarise(user_past_sentiment_avg = mean(user_past_sentiment_avg, na.rm = TRUE)) %>%
+  arrange(desc(user_past_sentiment_avg)) %>%
+  slice(1)
+
+print(senti_level)
+
+# Which campaign or brand shows the most positive buzz change rate over time?
+campa_brand <- data %>%
+  group_by(brand_name, product_name, campaign_name, campaign_phase) %>%
+  summarise(sentiment_score = mean(campaign_name, na.rm = TRUE)) %>% 
+  arrange(desc(sentiment_score)) %>%
+  slice(1)
+
+print(campa_brand)
